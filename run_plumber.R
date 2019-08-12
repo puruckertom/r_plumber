65919487378742f29d9bd1a4f7414ebc7c2eded2
Tom Purucker <purucker.tom@gmail.com>
2019-08-12 01:31:04 -0400

    r comments for readability

--- run_plumber.R
+++ run_plumber.R
@@ -1,16 +1,26 @@
+# plumber.R
+
 install.packages("plumber")
 library(plumber)
 
+# my machine's name is:
+Sys.info()[4]
+
+#running locally
+#modify this for your path with the machine name and
+#the repo directory
 if(Sys.info()[4] == "LZ2626UTPURUCKE"){
   pcdir <- path.expand("c:/git/r_plumber/")
 }
 
+#create a plumber instance
 plumber_filename <- paste(pcdir,"plumber.R",sep="")
 pr <- plumber::plumb(plumber_filename)
 
+#stand up plumber
 pr$run()
 
-# serving locally on
+# serving locally on (rstudio)
 # http://127.0.0.1:3958/__swagger__/
 # http://127.0.0.1:3958/echo
 # http://127.0.0.1:3958/plot
